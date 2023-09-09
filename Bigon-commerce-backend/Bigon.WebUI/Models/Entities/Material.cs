ea597f8970670e0216b954e7b2c46b722858f790
elishali <125192504+elishali@users.noreply.github.com>
2023-09-09 08:40:16 +0400

    greate  tables

---
+++ Bigon-commerce-backend/Bigon.WebUI/Models/Entities/Material.cs
@@ -0,0 +1,10 @@
+﻿using Bigon.WebUI.Models.Entities.Commons;
+
+namespace Bigon.WebUI.Models.Entities
+{
+    public class Material : BaseEntity<int>
+    {
+        public string Name { get; set; }
+
+    }
+}
---
+++ Bigon-commerce-backend/Bigon.WebUI/Models/Persistences/Configurations/MaterialEntityConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Bigon.WebUI.Models.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bigon.WebUI.Models.Persistences.Configurations
+{
+    public class MaterialEntityConfiguration : IEntityTypeConfiguration<Material>
+    {
+        public void Configure(EntityTypeBuilder<Material> builder)
+        {
+            builder.Property(m => m.Id).UseIdentityColumn(1, 1);
+            builder.Property(m => m.Name).HasColumnType("nvarchar").HasMaxLength(200).IsRequired();
+            builder.ConfigureAsAuditable();
+
+
+            builder.HasKey(m => m.Id);
+            builder.ToTable("Materials");
+        }
+    }
+}
