18339a3eb3f7cdde7025004e0cfcf145e13fc96f
elishali <125192504+elishali@users.noreply.github.com>
2023-09-14 02:30:07 +0400

    Blog FrontToBack Shop FrontToBack

--- Bigon-commerce-backend/Bigon.WebUI/Program.cs
+++ Bigon-commerce-backend/Bigon.WebUI/Program.cs
@@ -9,10 +9,13 @@ namespace Bigon.WebUI
         {
             var builder = WebApplication.CreateBuilder(args);
             builder.Services.AddControllersWithViews();
+           
             builder.Services.AddDbContext<DataContext>(cfg =>
             {
                 cfg.UseSqlServer(builder.Configuration.GetConnectionString("cString"));
             });
+
+            builder.Services.AddRouting(cfg => cfg.LowercaseUrls = true);
 
             var app = builder.Build();
 
