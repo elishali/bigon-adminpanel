6cb8d6cc62b150b240b898a89478c83e9838d791
elishali <125192504+elishali@users.noreply.github.com>
2023-10-06 21:11:53 +0400

    subscribe front functionality-done!

--- Bigon-commerce-backend/Bigon.WebUI/Controllers/HomeController.cs
+++ Bigon-commerce-backend/Bigon.WebUI/Controllers/HomeController.cs
@@ -16,5 +16,20 @@ namespace Bigon.WebUI.Controllers
         {
             return View();
         }
+
+        [HttpPost]
+        public IActionResult Subscribe(string email)
+        {
+            return Json(new
+            {
+                error=false,
+                email
+            });
+        }   
+
+        public IActionResult SubscribeApprove()
+        {
+            return Content("Abone oldunuz");
+        }
     }
 }
